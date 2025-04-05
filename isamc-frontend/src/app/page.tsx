f21d751acfd78f4706f91f1217245d8dff7439ba
YojansC27M <yojans555cardenas@gmail.com>
2025-04-04 20:46:36 -0500

    CrearProyectoNext

---
+++ isamc-frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function HomePage() {
+  return (
+    <>
+      <h1>Bienvenido al inicio</h1>
+      <p>Este es el contenido que se renderiza dentro del Main al lado del Drawer.</p>
+    </>
+  );
+}
